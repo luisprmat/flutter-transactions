bd8682c6215271a5a11be114aedccf7fd024921d
Luis Parrado <luisprmat@gmail.com>
2025-04-01 18:56:05 -0500

    Creating Our First Provider

---
+++ lib/providers/category_provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_transactions/models/category.dart';
+import 'package:flutter_transactions/services/api.dart';
+
+class CategoryProvider extends ChangeNotifier {
+  List<Category> categories = [];
+  late ApiService apiService;
+
+  CategoryProvider() {
+    apiService = ApiService();
+    init();
+  }
+
+  Future init() async {
+    try {
+      categories = await apiService.fetchCategories();
+    } catch (e) {
+      print('Failed to load categories: $e');
+    }
+
+    notifyListeners();
+  }
+
+  Future updateCategory(Category category) async {
+    try {
+      Category updatedCategory = await apiService.saveCategory(
+        category.id,
+        category.name,
+      );
+      int index = categories.indexOf(category);
+      categories[index] = updatedCategory;
+    } catch (e) {
+      print('Failed to update category: $e');
+    }
+  }
+}
