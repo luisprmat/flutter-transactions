b7e03863a6e833aacf291a3d791cd86a70e7bdc4
Luis Parrado <luisprmat@gmail.com>
2025-04-01 16:30:20 -0500

    Editing Categories

--- lib/screens/categories/categories_list.dart
+++ lib/screens/categories/categories_list.dart
@@ -13,6 +13,9 @@ class CategoriesList extends StatefulWidget {
 
 class CategoriesListState extends State<CategoriesList> {
   Future<List<Category>>? futureCategories;
+  final _formKey = GlobalKey<FormState>();
+  late Category selectedCategory;
+  final categoryNameController = TextEditingController();
 
   Future<List<Category>> fetchCategories() async {
     final http.Response response = await http.get(
@@ -29,6 +32,26 @@ class CategoriesListState extends State<CategoriesList> {
     List categories = data['data'];
 
     return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  Future saveCategory() async {
+    final form = _formKey.currentState;
+
+    if (!form!.validate()) {
+      return;
+    }
+
+    String url = 'http://10.0.2.2:8000/api/categories/${selectedCategory.id}';
+
+    final http.Response response = await http.put(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{'name': categoryNameController.text}),
+    );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -50,15 +73,59 @@ class CategoriesListState extends State<CategoriesList> {
               itemBuilder: (context, index) {
                 Category category = snapshot.data![index];
 
-                return ListTile(title: Text(category.name));
+                return ListTile(
+                  title: Text(category.name),
+                  trailing: IconButton(
+                    onPressed: () {
+                      selectedCategory = category;
+                      categoryNameController.text = category.name;
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            padding: EdgeInsets.all(20),
+                            child: Form(
+                              key: _formKey,
+                              child: Column(
+                                children: [
+                                  Text('Edit Category'),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Category Name',
+                                    ),
+                                    controller: categoryNameController,
+                                    validator: (String? value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter category name';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 20),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        saveCategory();
+                                      },
+                                      child: Text('Update'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(Icons.edit),
+                  ),
+                );
               },
             );
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
+          return Center(child: CircularProgressIndicator());
         },
       ),
     );
