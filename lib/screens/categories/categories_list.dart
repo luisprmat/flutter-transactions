bd79c11e22e8d48fec8bb9e4d10280f0546307fe
Luis Parrado <luisprmat@gmail.com>
2025-04-01 15:23:38 -0500

    First API Call - Categories List

--- lib/screens/categories/categories_list.dart
+++ lib/screens/categories/categories_list.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_transactions/models/category.dart';
+import 'package:http/http.dart' as http;
 
 class CategoriesList extends StatefulWidget {
   const CategoriesList({super.key});
@@ -8,37 +12,51 @@ class CategoriesList extends StatefulWidget {
 }
 
 class CategoriesListState extends State<CategoriesList> {
-  final List<String> categories = [
-    'Electronics',
-    'Fashion',
-    'Education',
-    'Home & Garden',
-    'Sporting Goods',
-  ];
+  Future<List<Category>>? futureCategories;
 
-  int clicked = 0;
+  Future<List<Category>> fetchCategories() async {
+    final http.Response response = await http.get(
+      Uri.parse('https://tight-optimum-weasel.ngrok-free.app/api/categories'),
+    );
+
+    final Map<String, dynamic> data = json.decode(response.body);
+
+    if (!data.containsKey('data') || data['data'] is! List) {
+      throw Exception('Failed to load categories');
+    }
+
+    List categories = data['data'];
+
+    return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureCategories = fetchCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Categories clicked $clicked times')),
-      body: Container(
-        color: Theme.of(context).primaryColor,
-        child: Center(
-          child: ListView.builder(
-            itemCount: categories.length,
-            // Must be the lenght of the list
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(
-                  categories[index],
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () => setState(() => clicked++),
-              );
-            },
-          ),
-        ),
+      appBar: AppBar(title: Text('Categories List')),
+      body: FutureBuilder<List<Category>>(
+        future: futureCategories,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                Category category = snapshot.data![index];
+
+                return ListTile(title: Text(category.name));
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
