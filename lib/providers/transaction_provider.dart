bc42a945308b3b608410571b39dd339220c4357e
Luis Parrado <luisprmat@gmail.com>
2025-04-03 22:44:34 -0500

    Creating Transaction Provider + Fix token type issue

---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_transactions/models/transaction.dart';
+import 'package:flutter_transactions/providers/auth_provider.dart';
+import 'package:flutter_transactions/services/api.dart';
+
+class TransactionProvider extends ChangeNotifier {
+  List<Transaction> transactions = [];
+  late ApiService apiService;
+  late AuthProvider authProvider;
+
+  TransactionProvider(AuthProvider authProvider) {
+    authProvider = authProvider;
+    apiService = ApiService(authProvider.token);
+    init();
+  }
+
+  Future init() async {
+    apiService = ApiService(await authProvider.getToken());
+    transactions = await apiService.fetchTransactions();
+    notifyListeners();
+  }
+
+  Future<void> addTransaction(
+    String amount, String category, String description, String date) async {
+    try {
+      Transaction addedTransaction =
+        await apiService.addTransaction(amount, category, description, date);
+      transactions.add(addedTransaction);
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
+ 
+  Future<void> updateTransaction(Transaction transaction) async {
+    try {
+      Transaction updatedTransaction =
+        await apiService.updateTransaction(transaction);
+      int index = transactions.indexOf(transaction);
+      transactions[index] = updatedTransaction;
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
+ 
+  Future<void> deleteTransaction(Transaction transaction) async {
+    try {
+      await apiService.deleteTransaction(transaction.id);
+      transactions.remove(transaction);
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
